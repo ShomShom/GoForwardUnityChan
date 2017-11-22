4cb442b20100d1cd96bad8df0c6e0c4c87501993
Sho Miyashita <siou_miyashita@asagi.waseda.jp>
2017-11-22 11:06:50 +0900

    GoForwardUnityChan
    課題提出1回目、ブロック着地音

---
+++ Assets/GroundSound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSound: MonoBehaviour {
+
+	public AudioClip audioClip;
+	AudioSource audioSource;
+
+	void Start() {
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D (Collision2D collision){
+		if (collision.gameObject.tag == "Block") {
+			audioSource.PlayOneShot(audioClip);
+		}
+	}
+
+}
